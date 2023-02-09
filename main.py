03df165b3fcea79e1dccd7a34dfe8104b6281f7b
rojcandraz <andraz.rojc@caretronic.com>
2023-02-09 13:36:17 +0100

    Prvo posiljanje

---
+++ main.py
@@ -0,0 +1,24 @@
+#! py -3
+
+import wmi
+
+def main():
+    rval = 0  # Default: Check passes.
+
+    # Initialize WMI objects and query.
+    wmi_o = wmi.WMI('.')
+    wql = ("SELECT * FROM Win64_NTLogEvent WHERE Logfile="
+           "'System' AND EventCode='1076'")
+
+    # Query WMI object.
+    wql_r = wmi_o.query(wql)
+
+    if len(wql_r):
+        rval = -1  # Check fails.
+
+    return rval
+
+
+
+if __name__ == '__main__':
+    main()
